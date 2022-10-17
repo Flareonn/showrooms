65f22c54177b57d94c8584bd1c4a28a793caa632
Павел Брюханов <appavel2011@yandex.ru>
2022-10-17 22:37:32 +0500

    Обновление составной функции, исключение пустых значений из запроса

--- src/mixins/router.ts
+++ src/mixins/router.ts
@@ -1,6 +1,14 @@
+import { omitEmptyValues, toStringValues } from "@/mixins";
 import router from "@/plugins/router";
 
-export const appendQuery = (params: Record<string, string | number>) =>
+export const appendQuery = (
+  params: Record<string, string | number | string[]>
+) => {
   router.push({
-    query: Object.assign({}, router.currentRoute.value.query, params),
+    query: toStringValues(
+      omitEmptyValues(
+        Object.assign({}, router.currentRoute.value.query, params)
+      )
+    ),
   });
+};
