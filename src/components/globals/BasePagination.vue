23f5a0bae607044f0df563e3ebb85c7124ebc190
Павел Брюханов <appavel2011@yandex.ru>
2022-10-18 22:31:35 +0500

    Правка компонента пагинации

--- src/components/globals/BasePagination.vue
+++ src/components/globals/BasePagination.vue
@@ -1,36 +1,47 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useRoute } from "vue-router"
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import { mergeQuery } from "@/mixins/router";
 interface IProps {
-  totalPages: number
+  totalPages: number;
 }
 defineProps<IProps>();
 const emit = defineEmits<{
-  (e: 'change', page: number): void
+  (e: "change", page: number): void;
 }>();
+
+const route = useRoute();
+
 const currentPage = computed(() => {
   let currentPage = 1;
-  const { page } = useRoute().query
-  if(page && +page >= 0) {
+  const { page } = route.query;
+  if (page && +page >= 0) {
     currentPage = +page;
   }
-  emit('change', currentPage)
+  emit("change", currentPage);
   return currentPage;
-})
+});
+const navigateTo = computed(() => {
+  return {
+    prev: mergeQuery({ page: `${currentPage.value - 1}` }),
+    page: (number: number) => mergeQuery({ page: `${number}` }),
+    next: mergeQuery({ page: `${currentPage.value + 1}` }),
+  };
+});
 </script>
 
 <template>
   <ul v-if="totalPages > 1" class="pagination">
     <li v-show="currentPage > 1">
-      <router-link :to="`?page=${currentPage - 1}`">Предыдущая</router-link>
+      <router-link :to="navigateTo.prev">Предыдущая</router-link>
     </li>
     <template v-if="totalPages > 2">
       <li v-for="number in totalPages" :key="number">
-        <router-link :to="`?page=${number}`">{{ number }}</router-link>
+        <router-link :to="navigateTo.page(number)">{{ number }}</router-link>
       </li>
     </template>
     <li v-show="currentPage < totalPages">
-      <router-link :to="`?page=${currentPage + 1}`">Следующая</router-link>
+      <router-link :to="navigateTo.next">Следующая</router-link>
     </li>
   </ul>
 </template>
