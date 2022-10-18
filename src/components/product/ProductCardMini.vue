8f558dc6644314e470c64c2725ff6ae75b77bf58
Павел Брюханов <appavel2011@yandex.ru>
2022-10-19 00:22:08 +0500

    Миграция компонента с vue 2

--- src/components/product/ProductCardMini.vue
+++ src/components/product/ProductCardMini.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+interface IProps {
+  grid_img: string;
+  title: string;
+  description: string;
+  name: string;
+  link: string;
+}
+defineProps<IProps>();
+</script>
+
 <template>
   <div class="product-item">
     <div class="product-image">
@@ -11,34 +22,3 @@
     <a :href="link" target="_blank" class="stretched-link"></a>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'ProductCardMini',
-  /** @type {ShowroomItem} */
-  props: {
-    description: {
-      type: String,
-      required: false,
-      default: '',
-    },
-    // eslint-disable-next-line vue/prop-name-casing
-    grid_img: {
-      type: String,
-      required: true,
-    },
-    link: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-  },
-}
-</script>
