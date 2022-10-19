70c5c07d72b2392cdf120bdbc8b53493a255b92a
Павел Брюханов <appavel2011@yandex.ru>
2022-10-19 21:14:05 +0500

    Компонент контрола файла, из vue 2

---
+++ src/components/controls/InputFile.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="upload">
+    <label :for="id">{{ placeholder }}</label>
+    <input
+      v-bind="$attrs"
+      :id="id"
+      ref="file"
+      type="file"
+      name="file"
+      @change="onUpload"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputFile',
+  inheritAttrs: false,
+  props: {
+    id: {
+      type: [Number, String],
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: 'Загрузить',
+    },
+  },
+  methods: {
+    /** @param {Event} e */
+    onUpload(e) {
+      const [file] = e.target.files
+      this.$emit('change', URL.createObjectURL(file))
+    },
+    trigger() {
+      this.$refs.file.click()
+    },
+  },
+}
+</script>
