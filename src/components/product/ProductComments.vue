a1e62f2e5185c1d0acf5161500bdf40254f8cf11
Павел Брюханов <appavel2011@yandex.ru>
2022-10-19 21:23:41 +0500

    Комментарии продукта

---
+++ src/components/product/ProductComments.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import type { AxiosStatic } from "axios";
+import { ref, inject } from "vue";
+
+import InputFile from "@/components/controls/InputFile.vue";
+
+const text = "";
+const formInput = ref<HTMLTextAreaElement | null>(null);
+
+const axios = inject("axios") as AxiosStatic;
+const id = inject("productId");
+
+const comments: IResponse<UserComment> = (
+  await axios.get(`/comments/showroom/${id}/`)
+).data;
+</script>
+
+<template>
+  <div class="comment-wrapper">
+    <div class="container">
+      <div class="comment-header">
+        <h3>Comments</h3>
+        <button
+          class="comment-anchor"
+          @click="() => (formInput as HTMLTextAreaElement).focus()"
+        >
+          Оставить комментарий
+        </button>
+      </div>
+      <!-- Comments Start //-->
+      <div class="comment-list">
+        <ul>
+          <base-comment
+            v-for="(comment, idx) in comments.results"
+            :key="idx"
+            v-bind="comment"
+          />
+        </ul>
+      </div>
+      <!-- Pagination Start //-->
+      <base-pagination :total-pages="comments.count_pages" />
+      <!-- Pagination End //-->
+
+      <!-- Comment Form Start //-->
+      <div class="comment-form">
+        <div class="comment-box">
+          <div class="author-avatar">
+            <img
+              src="https://html.cwsthemes.com/liami/assets/images/resources/product-img1-14.jpg"
+              alt=""
+            />
+          </div>
+          <div class="form-body">
+            <div class="author-name">Логин пользователя</div>
+            <form id="send-comment" @submit.prevent="">
+              <textarea
+                ref="formInput"
+                v-model="text"
+                name="comment"
+                rows="3"
+                placeholder="Написать текст..."
+              ></textarea>
+              <input-file
+                id="images"
+                placeholder=""
+                accept="image/jpeg,image/png,image/gif"
+              />
+            </form>
+          </div>
+        </div>
+        <button type="submit" form="send-comment">Отправить комментарий</button>
+      </div>
+      <!-- Comment Form End //-->
+    </div>
+  </div>
+</template>
