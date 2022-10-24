0cafe5a7d1e531e40e24e1fbc3c674fcacc09e1d
Павел Брюханов <appavel2011@yandex.ru>
2022-10-24 23:01:16 +0500

    Возможность v-model на компоненте

--- src/components/controls/InputPassword.vue
+++ src/components/controls/InputPassword.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts" :inherit-attrs="false">
-import { ref, computed } from 'vue';
+import { ref, computed } from "vue";
+
+const emit = defineEmits<{
+  (e: "update:modelValue", text: string): void;
+}>();
 
 const isVisiblePassword = ref(false);
-const inputType = computed(() => isVisiblePassword.value ? 'text' : 'password')
+const inputType = computed(() =>
+  isVisiblePassword.value ? "text" : "password"
+);
 </script>
 
 <template>
   <div class="input-password">
-    <input :type="inputType" name="password" v-bind="$attrs" />
+    <input
+      :type="inputType"
+      name="password"
+      v-bind="$attrs"
+      :value="$attrs.modelValue"
+      @input="
+        emit('update:modelValue', ($event.target as HTMLInputElement).value)
+      "
+    />
     <button
       class="show-password"
       aria-label="Показать пароль"
