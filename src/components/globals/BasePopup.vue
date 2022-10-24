b3a97f55317acc124016f532a1973b618a813d8d
Павел Брюханов <appavel2011@yandex.ru>
2022-10-24 23:14:11 +0500

    Закрытие окна при размонтировании

--- src/components/globals/BasePopup.vue
+++ src/components/globals/BasePopup.vue
@@ -1,25 +1,32 @@
 <script setup lang="ts">
-import { ref, watch, useAttrs } from "vue";
+import { ref, watch, useAttrs, onUnmounted } from "vue";
 import watchAndClose from "@/utils/popup";
 const attrs = useAttrs();
 
 const isOpen = ref(false);
 const popup = ref<HTMLElement | null>(null);
 
-const close = () => (isOpen.value = false);
-const open = () => (isOpen.value = true);
+const close = () => {
+  isOpen.value = false;
+  document.body.classList.remove("modal-open");
+};
+const open = () => {
+  isOpen.value = true;
+  document.body.classList.add("modal-open");
+};
 defineExpose({ close, open });
 
 watch(
   popup,
   (value) => {
-    document.body.classList[value ? "add" : "remove"]("modal-open");
     if (value) {
       watchAndClose(isOpen, popup.value as HTMLElement, close);
     }
   },
   { immediate: true }
 );
+
+onUnmounted(close);
 </script>
 <template>
   <Teleport to="body" v-if="isOpen">
