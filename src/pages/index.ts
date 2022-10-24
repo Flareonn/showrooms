83f93a5c8eb8aef9868e358fd187725d3abaf9f4
Павел Брюханов <appavel2011@yandex.ru>
2022-10-24 19:40:06 +0500

    Страница подборок

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,13 +2,15 @@ import Home from "./Home.vue";
 import Category from "./Category.vue";
 import ErrorPage from "./Error.vue";
 import Showroom from "./Showroom.vue";
+import Selections from "./Selections/index.vue";
 
 const Pages = {
   Home,
   Category,
   ErrorPage,
   Showroom,
+  Selections,
 };
 
-export { Home, Category, ErrorPage, Showroom };
+export { Home, Category, ErrorPage, Showroom, Selections };
 export default Pages;
