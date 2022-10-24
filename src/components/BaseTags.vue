8b2cc5a4ccb83446c3b3fdd0d1d334ff0b9540d3
Павел Брюханов <appavel2011@yandex.ru>
2022-10-24 20:44:50 +0500

    Компонент тегов

---
+++ src/components/BaseTags.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface IProps {
+  items?: {
+    id: string | number;
+    name: string | number;
+  }[];
+  link: string;
+}
+withDefaults(defineProps<IProps>(), {
+  items: () => [],
+});
+</script>
+
+<template>
+  <div v-if="items.length" class="tags-wrapper">
+    <ul>
+      <li v-for="(item, idx) in items" :key="idx">
+        <router-link :to="`/${link}/${item.id}`">
+          {{ item.name }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
